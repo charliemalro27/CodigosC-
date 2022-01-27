b80983d72f469f84f930b90c033191c8ca417078
charliemalro27 <carlos.maldonado@itgranada.com>
2022-01-27 23:23:42 +0100

    Ejercico vectores Dia 3

---
+++ EjercicioVectores.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "string"
+
+using namespace std;
+
+int main(){
+
+	int suma[100];
+	int vector[100];
+	
+	for(int i=0;i<101;i++){
+		vector[i]=i;
+		cout<<vector[i]<<endl;
+
+	}
+
+}
