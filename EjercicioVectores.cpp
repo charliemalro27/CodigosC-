240daee5792439371ac1dc1c7e97e8cd15b05a59
charliemalro27 <carlos.maldonado@itgranada.com>
2022-01-27 23:53:30 +0100

    Bucle while contraseña

--- EjercicioVectores.cpp
+++ EjercicioVectores.cpp
@@ -5,13 +5,21 @@ using namespace std;
 
 int main(){
 
-	int suma[100];
+	/*int suma[100];
 	int vector[100];
-	
+
 	for(int i=0;i<101;i++){
 		vector[i]=i;
 		cout<<vector[i]<<endl;
+	*/
 
-	}
+	string animal="langosta";
+  string contrasena="";
+
+  while(contrasena!=animal){
+	cout<<"Introduce mi animal favorito!"<<endl;
+	cin>>contrasena;
+  }
+
 
 }
